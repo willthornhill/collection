a5ed4d3f37716a1c6dd3dd3f2ab20bcfe5862f4d
William Thornhill <will.thornhill@hotmail.co.uk>
2021-09-27 16:01:45 +0100

    Removed some redundant code

--- collection.php
+++ collection.php
@@ -19,19 +19,12 @@ $sets = $query->fetchAll();
 
 <div class="main">
     <div class="item">
-<!--        <h3>Name Placeholder</h3>-->
-<!--        <img src="placeholder" alt="placeholder image"/>-->
-<!--        <p>Set Number:</p>-->
-<!--        <p>Piece Count:</p>-->
-<!--        <p>Release Year:</p>-->
         <?php
             foreach ($sets AS $set){
                 echo "<h2>" .$set['setName']. "</h2>";
                 echo "<h4>Set Number: " .$set['setNumber']. "</h4>";
                 echo "<h4>Piece Count: " .$set['pieceCount']. "</h4>";
                 echo "<h4>Release Year: " .$set['releaseYear']. "</h4>";
-
-
             }
         ?>
     </div>
