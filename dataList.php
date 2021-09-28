f0cc0d8aa991a2249be182e1f049fe6accf8d905
William Thornhill <will.thornhill@hotmail.co.uk>
2021-09-28 16:02:06 +0100

    Changed how the dataList function works, now it concatenates into a string. Made working success unit test for dataList function

--- dataList.php
+++ dataList.php
@@ -2,17 +2,27 @@
 
 /**
  * Formats and lists the contents of each field
- * 
+ *
  * @param array $sets
  * @return mixed
  */
 
 function dataList(array $sets){
+    // create list as empty string
+    $list = '';
     foreach ($sets as $set) {
-        echo "<h2>" . $set['setName'] . "</h2>";
-        echo "<h4>Set Number: " . $set['setNumber'] . "</h4>";
-        echo "<h4>Piece Count: " . $set['pieceCount'] . "</h4>";
-        echo "<h4>Release Year: " . $set['releaseYear'] . "</h4>";
+        // concatenate each of these onto list
+        $list .= "<h2>" . $set['setName'] . "</h2>";
+        $list .= "<h4>Set Number: " . $set['setNumber'] . "</h4>";
+        $list .= "<h4>Piece Count: " . $set['pieceCount'] . "</h4>";
+        $list .= "<h4>Release Year: " . $set['releaseYear'] . "</h4>";
+        echo $list;
+
+
+//        echo "<h2>" . $set['setName'] . "</h2>";
+//        echo "<h4>Set Number: " . $set['setNumber'] . "</h4>";
+//        echo "<h4>Piece Count: " . $set['pieceCount'] . "</h4>";
+//        echo "<h4>Release Year: " . $set['releaseYear'] . "</h4>";
     }
     return $list;
 }
